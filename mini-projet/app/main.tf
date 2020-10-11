0de51944326245d2ab4854db8b100860dadb85ae
Thomas MARTIN <tmartin@live.fr>
2020-10-11 16:24:04 +0200

    Remove maximum hard-coded features.

--- mini-projet/app/main.tf
+++ mini-projet/app/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "YOUR-TERRAFORM-BACKEND"
+    bucket = "terraform-backend-thomas"
     key = "miniprojet.tfstate"
     region     = "us-east-2"
   }
@@ -12,33 +12,34 @@ terraform {
 
 module "ec2" {
     source = "../modules/ec2module"
+
     sg_name = module.securitygroup.name
-    aws_kp_name = "YOUR_AWS_KEYS_PAIR_NAME"
-    pk_filepath = "PATH/OF/YOUR/PRIVATE/KEY.pem"
-    provisioner_script = "deploy_nginx.sh"
-    instancetype = "t2.nano"
-    aws_common_tag = {
-      Name = "ec2-dev-miniprojet"
-    }
+    aws_kp_name = var.aws_kp_name
+    pk_filepath = var.pk_filepath
+    provisioner_script = var.ec2_provisioner_script
+    instancetype = var.ec2_instance_type
+    aws_common_tag = var.aws_common_tag
 }
 
 module "securitygroup" {
     source = "../modules/securitygroupmodule"
-    sg_name = "sg_miniprojet"
+
+    sg_name = var.security_group_name
 }
 
 module "ebs" {
   source = "../modules/ebsmodule"
-  size = 5
+
+  size = var.additional_ebs_size
   availability_zone = module.ec2.availability_zone
   instance_id = module.ec2.instance_id
   public_ip = module.pubip.public_ip
-  pk_filepath = "PATH/OF/YOUR/PRIVATE/KEY.pem"
-  provisioner_script = "mount_ecb.sh"
+  pk_filepath = var.pk_filepath
+  provisioner_script = var.ebs_provisioner_script
 }
 
 module "pubip" {
   source = "../modules/pubipmodule"
+
   instance_id = module.ec2.instance_id
 }
-
